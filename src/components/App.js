6208e52c5466d3e1b2f5a1e2bdaffea9dab0c787
Daniel Tossou <tossdaniel@gmail.com>
2021-10-20 17:20:48 +0000

    Added components folder Deleted files in src folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          We start coding !
         </p>
         <a
           className="App-link"
